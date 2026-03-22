976113b1b74ae0d04e32d02127d9657ee3b7d7c2
Divyanshii-Rajput <divyanshirajput02003@gmail.com>
2026-03-22 23:24:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get("/api/feedback", (req, res) => {
   res.json(feedbacks);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
